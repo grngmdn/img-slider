1555aca2a9d9eed685f012badf038d3d5fb4adc3
Madan gurung <gurungmadan@hotmail.com>
2022-02-28 03:57:17 +0000

    adding MUI styles to the app

--- src/App.js
+++ src/App.js
@@ -1,33 +1,55 @@
 import React, { useState } from "react"
 import data from "./Data"
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import { Button, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 
 function App() {
 
   const [people, setPeople] = useState(data)
+  const [index, setIndex] = useState(0)
 
   return (
-    <div>
-      <h1>/ Reviews</h1>
-      <div>
+    <Box>
+      <Typography
+        variant="h4"
+      >/ Reviews
+      </Typography>
+      <Box>
         {people.map(
           (person, personIndex) => {
 
             const {id, image, name, title, quote} = person
             return(
-              <div key={id}>
+              <Box key={id}>
                 <img src={image} width="150px"/>
-                <h4>{name}</h4>
-                <p>{title}</p>
-                <p>{quote}</p>
-              </div>
+                <Typography
+                  variant="h6"
+                >{name}
+                </Typography>
+                <Typography
+                  variant="p"
+                >{title}
+                </Typography>
+                <Typography
+                  variant="p"
+                >{quote}
+                </Typography>
+                <FaQuoteRight />
+              </Box>
             )
           }
         )}
-      </div>
-      <button>prev</button>
-      <button>next</button>
+      </Box>
+      <Button>
+        <FiChevronLeft />
+      </Button>
+      <Button>
+        <FiChevronRight />
+      </Button>
 
-    </div>
+    </Box>
   );
 }
 
